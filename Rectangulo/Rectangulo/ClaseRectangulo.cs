2afaaadfa78500fe7b19a35a03dd4ed03b96b1e1
Ada Jensy Sorto Tinoco <adajensy@outlook.com>
2019-06-09 21:10:54 -0600

    Agregar public string  por AX

---
+++ Rectangulo/Rectangulo/ClaseRectangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangulo
+{
+    class ClaseRectangulo
+    {
+        private string ax;
+        private string ay;
+
+        private string bx;
+        private string by;
+
+        private string cx;
+        private string cy;
+
+        private string dx;
+        private string dy;
+
+        private string altura;
+        private string base1;
+        private string primetro;
+        private string area;
+        private bool validar;
+        private string resustado;
+
+        public string AX
+        {
+            get { return ax; }
+            set
+            {
+                int numero;
+                bool resultado = int.TryParse(value, out numero);
+                if (resultado) ax = value;
+                OnPropertyChanged("AX");
+                OnPropertyChanged("Altura");
+                OnPropertyChanged("Base");
+            }
+        }
+    }
+}
