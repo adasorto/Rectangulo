c216612a014a1ee1e99fc518d56b3a5e81dbf249
Ada Jensy Sorto Tinoco <adajensy@outlook.com>
2019-06-09 22:45:15 -0600

    Agregar Public string por perimetro, area, base y altura

--- Rectangulo/Rectangulo/ClaseRectangulo.cs
+++ Rectangulo/Rectangulo/ClaseRectangulo.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+
 
 namespace Rectangulo
 {
-    class ClaseRectangulo
+    class ClaseRectangulo : INotifyPropertyChanged
     {
         private string ax;
         private string ay;
@@ -24,7 +27,7 @@ namespace Rectangulo
         private string base1;
         private string primetro;
         private string area;
-        private bool validar;
+        private bool validacion;
         private string resustado;
 
         public string AX
@@ -125,6 +128,191 @@ namespace Rectangulo
             }
         }
 
+        public string DY
+        {
+            get { return dy; }
+            set
+            {
+                int numero;
+                bool resultado = int.TryParse(value, out numero);
+                if (resultado) dy = value;
+                OnPropertyChanged("DY");
+                OnPropertyChanged("Altura");
+                OnPropertyChanged("Base");
+            }
+        }
 
+
+        public bool distancia()
+        {
+            {
+
+
+                Double ab, dc, da, cb;
+                Double ab2, dc2, da2, cd2;
+
+                ab = Math.Pow((int.Parse(AX) - int.Parse(BX)), 2) + Math.Pow((int.Parse(AY) - int.Parse(BY)), 2);
+                ab2 = Math.Pow(Math.Sqrt(ab), 2);
+                dc = Math.Pow((int.Parse(DX) - int.Parse(CX)), 2) + Math.Pow((int.Parse(DY) - int.Parse(CY)), 2);
+                dc2 = Math.Sqrt(dc);
+                da = Math.Pow((int.Parse(DX) - int.Parse(AX)), 2) + Math.Pow((int.Parse(DY) - int.Parse(AY)), 2);
+                da2 = Math.Sqrt(da);
+                cb = Math.Pow((int.Parse(CX) - int.Parse(BX)), 2) + Math.Pow((int.Parse(CY) - int.Parse(BY)), 2);
+                cd2 = Math.Sqrt(cb);
+
+                if (ab2 != dc2 && da2 != cd2)
+                {
+                    MessageBox.Show("No se puede Crear el Rectangulo!");
+                    validacion = false;
+                }
+                else
+                {
+                    // MessageBox.Show("Se creo el Rectangulo");
+                    validacion = true;
+                }
+
+                return validacion;
+            }
+        }
+
+        public string Base
+        {
+            get
+            {
+
+                if (distancia() != false)
+                {
+                    double ab, ab2;
+                    ab = Math.Pow((int.Parse(AX) - int.Parse(BX)), 2) + Math.Pow((int.Parse(AY) - int.Parse(BY)), 2);
+
+                    ab2 = Math.Sqrt(ab);
+
+                    base1 = ab2.ToString();
+
+
+
+                }
+                return base1.ToString();
+            }
+            set
+            {
+
+
+                if (distancia() != false)
+                {
+                    Double ab, ab2;
+
+                    ab = Math.Pow((int.Parse(AX) - int.Parse(BX)), 2) + Math.Pow((int.Parse(AY) - int.Parse(BY)), 2);
+                    ab2 = Math.Sqrt(ab);
+
+                    base1 = ab2.ToString();
+                    OnPropertyChanged("Base");
+
+
+                }
+
+
+
+            }
+        }
+
+
+        public string Altura
+        {
+            get
+            {
+
+                if (distancia() != false)
+                {
+                    Double da, da2;
+
+                    da = Math.Pow((int.Parse(DX) - int.Parse(AX)), 2) + Math.Pow((int.Parse(DY) - int.Parse(AY)), 2);
+                    da2 = Math.Pow(Math.Sqrt(da), 2);
+
+
+                    altura = da2.ToString();
+
+
+
+                }
+                return altura.ToString();
+            }
+            set
+            {
+
+
+                if (distancia() != false)
+                {
+                    Double da, da2;
+
+
+                    da = Math.Pow((int.Parse(DX) - int.Parse(AX)), 2) + Math.Pow((int.Parse(DY) - int.Parse(AY)), 2);
+                    da2 = Math.Pow(Math.Sqrt(da), 2);
+
+                    altura = da2.ToString();
+                    OnPropertyChanged("Altura");
+
+
+                }
+
+
+
+            }
+        }
+
+        public string Perimetro
+        {
+            get
+            {
+
+                if (distancia() != false)
+                {
+                    double p;
+                    p = int.Parse(Base) * 2 + int.Parse(Altura) * 2;
+                    Perimetro = p.ToString();
+                }
+
+
+                return Perimetro;
+            }
+            set
+            {
+                double p;
+                p = int.Parse(Base) + int.Parse(Altura);
+                OnPropertyChanged("Perimetro");
+            }
+        }
+
+        public string Area
+        {
+            get
+            {
+
+                if (distancia() != false)
+                {
+                    double A;
+                    A = int.Parse(Base) + int.Parse(Altura);
+                    area = A.ToString();
+                }
+
+
+                return area;
+            }
+            set
+            {
+                double A;
+                A = int.Parse(Base) + int.Parse(Altura);
+                OnPropertyChanged("Area");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
     }
+   
 }
